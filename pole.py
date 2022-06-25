0f9c9e2a7e5a8d00733f676fe3ff3e43c1d2f5d6
wf <wrotki8778@gmail.com>
2022-06-25 15:46:11 +0200

    Add path simulation (no interaction so far)

--- pole.py
+++ pole.py
@@ -29,5 +29,27 @@ def dynamics(poles,pos,speed,dt=1/4,resistance=1/16):
     pos_next = pos + dt * speed
     return pos_next,speed_next
 
-print(dynamics(poles,np.array([1,0]),np.array([1,0])))
+def stop_condition(pole_borders,pos,speed,time):
+    out_of_bounds=  pole_borders[0,0]>pos[0] \
+                    or pole_borders[0,1]<pos[0] \
+                    or pole_borders[1,0]>pos[1] \
+                    or pole_borders[1,1]<pos[1]
+    timeout = time > 16
+    no_speed = np.linalg.norm(speed) < 1/64 and time > 1
+    return(no_speed or timeout or out_of_bounds)
+
+
+def path_simulation(poles,origin,dt=1/4,resistance=1/16):
+    time = 0
+    pos = origin
+    speed = np.array([0,0])
+    while (not(stop_condition(pole_borders,pos,speed,time))):
+        pos, speed = \
+            dynamics(poles,pos,speed,dt=dt,resistance=resistance)
+        time += dt
+    print("Simulation has ended after {} seconds. The point is located at \
+            {}, {} placement. Thank you for the participation!".format(time,pos[0],pos[1]))
+
+path_simulation(poles,np.array([0,0]),dt=1/8, resistance = 1/16)
+
 
