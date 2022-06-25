0c8819f09596ede9bcf7f8c749db110802bb651d
wf <wrotki8778@gmail.com>
2022-06-25 16:08:23 +0200

    Add plot features

--- pole.py
+++ pole.py
@@ -1,5 +1,6 @@
 import numpy as np
-pole_borders = np.array([[-1000,1000],[-1000,1000]], dtype=np.single)
+import matplotlib.pyplot as plt
+pole_borders = np.array([[-5,5],[-5,5]], dtype=np.single)
 origin = [0,0]
 
 def mountain(pos,center,magnitude):
@@ -14,10 +15,10 @@ def pole_equation(poles,point):
     return sum([pole(point) for pole in poles])
 
 def pole1(point):
-    return mountain(point,np.array([1,1]),40)
+    return mountain(point,np.array([1,1]),5)
 
 def pole2(point):
-    return mountain(point,np.array([2,-1]),30)
+    return mountain(point,np.array([2,-1]),8)
 
 poles = [pole1,pole2]
 
@@ -35,21 +36,25 @@ def stop_condition(pole_borders,pos,speed,time):
                     or pole_borders[1,0]>pos[1] \
                     or pole_borders[1,1]<pos[1]
     timeout = time > 16
-    no_speed = np.linalg.norm(speed) < 1/64 and time > 1
+    no_speed = np.linalg.norm(speed) < 1/16 and time > 1
     return(no_speed or timeout or out_of_bounds)
 
 
 def path_simulation(poles,origin,dt=1/4,resistance=1/16):
     time = 0
     pos = origin
+    hist_pos = pos
     speed = np.array([0,0])
     while (not(stop_condition(pole_borders,pos,speed,time))):
         pos, speed = \
             dynamics(poles,pos,speed,dt=dt,resistance=resistance)
         time += dt
+        hist_pos = np.vstack((hist_pos,pos))
     print("Simulation has ended after {} seconds. The point is located at \
-            {}, {} placement. Thank you for the participation!".format(time,pos[0],pos[1]))
-
-path_simulation(poles,np.array([0,0]),dt=1/8, resistance = 1/16)
+            {:4f}, {:4f} placement. The speed at the end was equal to {:4f}.\
+                 Thank you for the participation!".format(time,pos[0],pos[1],np.linalg.norm(speed)))
+    plt.plot(hist_pos[:,0],hist_pos[:,1], 'bo')
+    plt.savefig('foo.png')
+results = path_simulation(poles,np.array([1,0]),dt=1/2, resistance = 1/8)
 
 
