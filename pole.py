2288bb236f02ba0eaf45b9b25c36c48e048b5313
wf <wrotki8778@gmail.com>
2022-06-26 10:21:11 +0200

    Add obstacles and the boundaries as the classes

--- pole.py
+++ pole.py
@@ -1,9 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+class Environment:
+    def __init__(self, borders, origin):
+        self.borders = borders
+        self.origin = origin
+    def out_of_bounds(self,pos):
+        return(self.borders[0,0]>pos[0] \
+                    or self.borders[0,1]<pos[0] \
+                    or self.borders[1,0]>pos[1] \
+                    or self.borders[1,1]<pos[1])
 pole_borders = np.array([[-5,5],[-5,5]], dtype=np.single)
 origin = [0,0]
 
-def mountain(pos,center,magnitude):
+class Obstacle:
+    def __init__(self,equation,params,name):
+        self.equation = equation
+        self.params = params
+        self.name = name
+    def evaluate(self,pos):
+        return(self.equation(pos,self.params))
+
+def mountain(pos,params):
+    center = params[0]
+    magnitude = params[1]
     diff = pos-center
     if not diff.any():
         return np.array([0,0])
@@ -11,14 +31,11 @@ def mountain(pos,center,magnitude):
         distance = np.linalg.norm(diff)
         return np.exp(-distance**2)*magnitude*diff/distance
 
+pole1 = Obstacle(mountain,[np.array([1,1]),5],"first_mountain")
+pole2 = Obstacle(mountain,[np.array([2,-1]),8],"second_mountain")
+
 def pole_equation(poles,point):
-    return sum([pole(point) for pole in poles])
-
-def pole1(point):
-    return mountain(point,np.array([1,1]),5)
-
-def pole2(point):
-    return mountain(point,np.array([2,-1]),8)
+    return sum([pole.evaluate(point) for pole in poles])
 
 poles = [pole1,pole2]
 
@@ -30,22 +47,19 @@ def dynamics(poles,pos,speed,dt=1/4,resistance=1/16):
     pos_next = pos + dt * speed
     return pos_next,speed_next
 
-def stop_condition(pole_borders,pos,speed,time):
-    out_of_bounds=  pole_borders[0,0]>pos[0] \
-                    or pole_borders[0,1]<pos[0] \
-                    or pole_borders[1,0]>pos[1] \
-                    or pole_borders[1,1]<pos[1]
+def stop_condition(environment,pos,speed,time):
+    out_of_bounds=  environment.out_of_bounds(pos)
     timeout = time > 16
     no_speed = np.linalg.norm(speed) < 1/16 and time > 1
     return(no_speed or timeout or out_of_bounds)
 
 
-def path_simulation(poles,origin,dt=1/4,resistance=1/16):
+def path_simulation(poles,environment,origin,dt=1/4,resistance=1/16):
     time = 0
     pos = origin
     hist_pos = pos
     speed = np.array([0,0])
-    while (not(stop_condition(pole_borders,pos,speed,time))):
+    while (not(stop_condition(environment,pos,speed,time))):
         pos, speed = \
             dynamics(poles,pos,speed,dt=dt,resistance=resistance)
         time += dt
@@ -55,6 +69,9 @@ def path_simulation(poles,origin,dt=1/4,resistance=1/16):
                  Thank you for the participation!".format(time,pos[0],pos[1],np.linalg.norm(speed)))
     plt.plot(hist_pos[:,0],hist_pos[:,1], 'bo')
     plt.savefig('foo.png')
-results = path_simulation(poles,np.array([1,0]),dt=1/2, resistance = 1/8)
+
+environment = Environment(pole_borders,origin)
+
+results = path_simulation(poles,environment,np.array([1,0]),dt=1/8, resistance = 1/8)
 
 
