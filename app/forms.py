9a92757f85dfeb12d4520da7683a253f58dc741a
wf <wrotki8778@gmail.com>
2022-07-10 10:27:23 +0200

    Improve Flask stuff

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField\
-    ,BooleanField, SubmitField
+    ,DecimalField, SubmitField
 from wtforms.validators import DataRequired
 
 class form_test(FlaskForm):
-    position1 = StringField('Position 1')
+    position1 = DecimalField('Position 1')
+    position2 = DecimalField('Position 2')
+    speed_amplitude = DecimalField('Amplitude of speed')
+    speed_phase = DecimalField('Phase of speed')
     submit = SubmitField('Tap OK')
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,16 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import form_test
 
 @app.route('/', methods = ['GET','POST'])
-@app.route('/index')
+@app.route('/index', methods = ['GET','POST'])
 def index():
     form = form_test()
-    return(render_template('base.html',form=form))
+    if form.validate_on_submit():
+        flash('Position vector: ({},{}),\
+            vector initial speed: ({},{})'\
+                .format(form.position1.data,form.position2.data\
+                    ,form.speed_amplitude.data,form.speed_phase.data))
+        return redirect('/index')
+    return(render_template('base.html', \
+        title = 'Get parameters',form=form))
